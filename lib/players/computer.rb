16131e8db9f0670adafb17dc6e3f075d3b7dcb3f
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-31 23:23:13 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -11,6 +11,9 @@ module Players
         # If going second (and you took the middle square), and the opponent took a corner square, take a side square. 
       elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
         move = [2, 4, 6, 8].detect{|i| i.board.position(i+1)}.to_s 
+        
+      elsif board.turn_count == 4 && (board.position(2) == board.position(8) || board.position(4) == board.position(6))
+        move = [1, 3, 7, 9].detect{|i| i.board.position(i+1)}.to_s 
       
       else 
         Game::WIN_COMBINATIONS.detect do |win_combo|
