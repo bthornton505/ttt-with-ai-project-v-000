ddbfd62a24c401077ab91776cb65e46453f7bc35
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-31 22:54:34 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -18,7 +18,7 @@ module Players
       
       else 
         Game::WIN_COMBINATION.detect do |win_combo|
-          
+          move = [1, 3, 7, 9, 2, 4, 6, 8].detect{|i| !board.taken?(i)}.to_s if move == nil
       end 
         
         
