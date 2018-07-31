3006a9219394836155a5f9b4508410d8ab075a22
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-31 23:01:36 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -22,8 +22,8 @@ module Players
           if win_combo.select{|i| board.position(i+1) == token}.size == 2 && win_combo.any?{|i| board.position(i+1) == " "}
             move = win_combo.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
             
-            elsif cmb.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
-            move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+            elsif win_combo.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && win_combo.any?{|i| board.position(i+1) == " "}
+            move = win_combo.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
           end
         end
           move = [1, 3, 7, 9, 2, 4, 6, 8].detect{|i| !board.taken?(i)}.to_s if move == nil
