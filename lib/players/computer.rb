e1d78fce17e06f9c1fd414d8bff93c02c5a6fa97
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-31 23:01:08 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -26,7 +26,7 @@ module Players
             move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
           end
         end
-          
+          move = [1, 3, 7, 9, 2, 4, 6, 8].detect{|i| !board.taken?(i)}.to_s if move == nil
       end 
         
         
