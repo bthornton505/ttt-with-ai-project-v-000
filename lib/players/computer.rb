2ee8bc60d68e38616b4f0ed145d5fee0774ac5cf
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-31 23:25:50 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -12,8 +12,8 @@ module Players
       elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
         move = [2, 4, 6, 8].detect{|i| i.board.position(i+1)}.to_s 
         
-      elsif board.turn_count == 4 && (board.position(2) == board.position(8) || board.position(4) == board.position(6))
-        move = [1, 3, 7, 9].detect{|i| i.board.position(i+1)}.to_s 
+      # elsif board.turn_count == 4 && (board.position(2) == board.position(8) || board.position(4) == board.position(6))
+      #   move = [1, 3, 7, 9].detect{|i| i.board.position(i+1)}.to_s 
       
       else 
         Game::WIN_COMBINATIONS.detect do |win_combo|
